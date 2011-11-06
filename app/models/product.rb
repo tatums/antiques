c42236f305c521f8e81185460990178e5e65938f
Tatum Szymczak <t.szymczak@tukaiz.com>
2011-11-06 08:34:00 -0600

    Added back and status buttons to the backend

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+
   has_many :images, :dependent => :destroy
   has_many :measurements, :dependent => :destroy
   has_many :sliders
@@ -6,35 +7,27 @@ class Product < ActiveRecord::Base
   has_many :tags
   has_many :categories, :through => :tags
 
+  before_save :set_item_number
+
   validates_presence_of :title, :body, :image, :country, :period
-
-  after_create :set_item_number
-
  
   #attr_accessible :active, :title, :body, :new_acquisition, :phoebe_find, :country, :period, :image, :image_cache, :category_ids
   mount_uploader :image, ImageUploader
-
-  
 
   def to_param  # overridden
     "#{id}-#{title.gsub(/\W/, '-').downcase}"
   end  
 
-
   scope :active, where(:active => true)
-
   scope :phoebe_finds, where(:phoebe_find => true)
   scope :new_acquisitions, where(:new_acquisition => true)
 
 
-
+protected
   def set_item_number
-    if Product.all.empty?
-      number = 1000
-    else  
-      number = Product.last.item_number
-    end
-    update_attributes(:item_number => number)
+    number = Product.last.item_number ||= 1000
+    number += 1
+    self.item_number = number
   end
 
 
