ed8c6c68ca61f112c58b04a472e9fa6bca7bfdb0
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-04-20 18:51:43 -0500

    Add Strikethrough to tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+  def show_title(task)
+    if task.completed?
+      "<s>#{link_to task.title, task}</s>".html_safe
+    else
+      link_to task.title, task
+    end
+  end
+
 end
