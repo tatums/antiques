c1f4ad22348c6848218e543f1d20ea922fab9322
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-07-15 08:17:47 -0500

    fix conflicting css .widget and remove old files

--- app/helpers/visitor_view_helper.rb
+++ app/helpers/visitor_view_helper.rb
@@ -1,10 +1,2 @@
 module VisitorViewHelper
-  def visitor_view_link
-    if visitor_view
-      link_to "Disable Visitor View", disable_visitor_view_path, method: 'post', class: 'btn btn-small btn-primary'
-    else
-      link_to "Enable Visitor View", enable_visitor_view_path, method: 'post', class: 'btn btn-small btn-primary'
-    end
-  end
-
 end
