2a44ec9e9f49a8ff0ab04afd0c2946aa2192e134
tatumszymczak <tatum@ashlandstudios.com>
2012-12-15 14:50:40 -0600

    removed some comments

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,25 +2,16 @@ class Keyword < ActiveRecord::Base
   belongs_to :product, :touch => true
   after_save :create_associated_product_index
   after_destroy :destroy_associated_product_index
-  
+
   validates :title, :presence => true
-  
-  # def split_sentence_and_save_each_word_as_a_keyword(sentence, product)
-  #   words = sentence.split(" ")
-  #   words.each do |word|
-  #     keyword = product.keyword.build
-  #     keyword.title = word
-  #     keyword.save
-  #   end  
-  # end  
-  
+
   private
   def create_associated_product_index
     product.index!
   end
-  
+
   def destroy_associated_product_index
     product.index!
   end
-  
+
 end
