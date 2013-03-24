c871f28c55724f7695f852a4960c7c38b7bfa6cc
tatums <tatum@ashlandstudios.com>
2013-03-24 09:46:15 -0500

    refactor controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,51 +3,46 @@ class LineItemsController < ApplicationController
 
   def edit
     @line_item = LineItem.find(params[:id])
+    respond_with(@line_item)
   end
 
   def update
     @line_item = LineItem.find(params[:id])
-    respond_to do |format|
-      if @line_item.update_attributes(params[:line_item])
-        format.html { redirect_to @line_item.invoice, notice: 'Line Item was successfully updated.' }
-      else
-        format.html { render action: "edit" }
-      end
+    if @line_item.update_attributes(params[:line_item])
+      flash[:notice] = 'Line Item was successfully updated.'
     end
+    respond_with(@line_item)
   end
 
   def create
     @invoice = Invoice.find(params[:invoice_id])
-    if params[:product_id]
-      find_product_and_set_params
-    else
-      @line_item = @invoice.line_items.build(params[:line_item])
+    @line_item = setup_line_item
+    if @line_item.save
+      flash[:notice] = 'Line Item was successfully created.'
     end
-    respond_to do |format|
-      if @line_item.save
-        format.js
-      else
-        format.js {redirect_to @line_item.invoice, notice: 'Line Item was NOT successfully created.'}
-      end
-    end
+    respond_with(@line_item)
   end
 
   def destroy
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
-    respond_to do |format|
-      format.html { redirect_to @line_item.invoice }
-      format.js
-    end
+    respond_with(@line_item, location: @line_item.invoice)
   end
 
 private
 
+  def setup_line_item
+    if params[:product_id]
+      find_product_and_set_params
+    else
+      @invoice.line_items.build(params[:line_item])
+    end
+  end
+
   def find_product_and_set_params
     @product = Product.find(params[:product_id])
     @product.set_as_sold_and_move_to_sold_category
-    @line_item = @invoice.setup_line_item_from(@product)
+    @invoice.setup_line_item_from(@product)
   end
-
 
 end
