fb09b535baa142e79fc919fda4abb54f53d7694f
tatums <tatum@ashlandstudios.com>
2013-10-20 08:13:31 -0500

    add jquery ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,9 +5,17 @@
 // the compiled file.
 //
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
 //= require fancybox
 //= require twitter/bootstrap
 //= require jquery-fileupload/basic
+//= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.mouse
+//= require jquery.ui.position
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
+//= require jquery.ui.resizable
+//= require jquery.ui.selectable
+//= require jquery.ui.sortable
 //= require_tree .
